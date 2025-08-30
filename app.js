a493bae7c116b3aa064563bd8ffa8cfabd6743bc
Bhabesh Guchhait <bhabesh2698@gmail.com>
2025-08-30 20:08:52 +0530

    Update

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const express = require('express');
-const app = express();
-app.listen(3000,()=>{
-    console.log("3000");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(`${new Date().toISOString()} ${req.method} ${req.url}`);
+
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("My first node server is running ......")
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Node server is running on https://localhost:${port}`);
+
 })
